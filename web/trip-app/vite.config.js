be82a35dc97c265a636220fc30fe65a3c966e776
Rose-r1 <renkaixin167@gmail.com>
2026-03-02 14:26:33 +0800

    配置修改

--- web/trip-app/vite.config.js
+++ web/trip-app/vite.config.js
@@ -9,4 +9,10 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
+  server: {
+    host: '0.0.0.0', // 监听所有地址，包括局域网和本地 IPv4
+    // 或者
+    // host: 'localhost', 
+    port: 5173,
+  },
 })
