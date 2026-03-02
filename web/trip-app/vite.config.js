a099cdd75a68312514b5604640b141e910fdf029
Rose-r1 <renkaixin167@gmail.com>
2026-03-02 14:38:07 +0800

    更新

--- web/trip-app/vite.config.js
+++ web/trip-app/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import path, { basename } from 'path'
 
 export default defineConfig({
-  base: '/Major-Assignment/',
   plugins: [react()],
   resolve: {
     alias: {
